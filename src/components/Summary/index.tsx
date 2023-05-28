76635f7678b0e43887ecd38fd4eb776f043499e7
Gustavo_Siqueira <gustavo@id360.com.br>
2023-05-28 17:55:58 -0300

    criando custom hooke

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,3 @@
-import { useContext } from 'react'
-import { TransactionsContext } from '../../contexts/TransactionsContext'
 import { Container } from '../../styles/global'
 import { SummaryContent, SummaryContainer, SummaryCard } from './style'
 import {
@@ -8,23 +6,10 @@ import {
   BsCurrencyDollar,
 } from 'react-icons/bs'
 import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export const Summary = () => {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
-      return accumulator
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
