4993502a6a60318caff245db39d8bafe90f04335
Gustavo_Siqueira <gustavo@id360.com.br>
2023-06-01 02:58:41 -0300

    .

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 // export const api = axios.create({ baseURL: 'http://localhost:3333' })
+
+export const api = axios.create({
+  baseURL: 'https://dtmoney-server-test.vercel.app/api',
+})
+
+/*
 export const api = axios.create({
   // API SIMULADA (https://www.mockapi.io/)
   baseURL: 'https://6477ed56362560649a2d0c36.mockapi.io',
 })
+*/
